b169a2e7ac40761c76fbdafe248b6028bb5aea86
Jake Hickenlooper <jake@weboftomorrow.com>
2017-08-16 05:37:55 -0600

    Show better example of pile shuffle

--- src/pile/example.js
+++ src/pile/example.js
@@ -9,14 +9,39 @@ window.customElements.whenDefined(crdtrxPile)
   .then((Pile) => {
     // Pile is now defined and could be used to create pile elements.
 
+    // Create 52 example elements that are in a random order
+    const exampleList = Pile.randomListOfNumbers(52).map((value, index) => {
+      const div = document.createElement('div')
+      div.setAttribute('id', `example-div-${index}`)
+      div.setAttribute('class', 'example-div')
+      div.textContent = index
+      div.style.zIndex = value
+      return div
+    })
+    const exampleDivContainer = document.createDocumentFragment()
+    exampleList.forEach((div) => {
+      exampleDivContainer.appendChild(div)
+    })
+    examplePile1.querySelector('[slot=area]').appendChild(exampleDivContainer)
+
     // Listen for the shuffle event on a pile
     exampleContainerElement.addEventListener('crdtrx-pile-shuffle', shuffle, true)
     function shuffle (ev) {
+      // list contains the list of random integers
       const list = ev.detail.list
       const divListInPile = ev.target.querySelectorAll('.example-div')
       divListInPile.forEach((div) => {
-        console.log(div)
-        div.style.zIndex = list.shift()
+        const z = list.shift()
+        const prevZ = div.style.zIndex
+
+        // Shift all to the right
+        div.style.transform = `translate3d(${200 + (prevZ * 15)}px, ${prevZ * 15}px, 0)`
+
+        // One by one move a random one back
+        window.setTimeout(() => {
+          div.style.zIndex = z
+          div.style.transform = `translate3d(${z * 15}px, ${z * 15}px, 0)`
+        }, (prevZ * 50) + 1000)
       })
     }
 
@@ -25,6 +50,8 @@ window.customElements.whenDefined(crdtrxPile)
     if (shuffleButton) {
       shuffleButton.addEventListener('click', function handleClickShuffleButton (ev) {
         const divListInPile = examplePile1.querySelectorAll('.example-div')
+
+        // Call the shuffle method with the count of random integers that will be added to the event.detail.
         examplePile1.shuffle(divListInPile.length)
       }, true)
     }
--- src/pile/pile.component.js
+++ src/pile/pile.component.js
@@ -9,8 +9,8 @@ ${template}
 
 /**
  * A pile of elements
- * Properties:
- * - stackType: neat, vertical, horizontal, as-is
+ * Properties?:
+ * - stackType?: neat, vertical, horizontal, as-is
  */
 class Pile extends window.HTMLElement {
   constructor () {
@@ -18,16 +18,9 @@ class Pile extends window.HTMLElement {
     const shadowRoot = this.attachShadow({mode: 'open'})
     shadowRoot.innerHTML = html
 
-    // Set the action for the add-card button
-    // shadowRoot.querySelectorAll('[data-card]').forEach((button) => {
-    //   button.addEventListener('mousedown', this.handleMousedown.bind(this))
-    // })
-
-    this.area = shadowRoot.getElementById('area')
-    this.areaSlot = this.querySelector('[slot=area]')
-    this.footprint = shadowRoot.getElementById('footprint')
-
-    this.count = 0
+    // this.area = shadowRoot.getElementById('area')
+    // this.areaSlot = this.querySelector('[slot=area]')
+    // this.footprint = shadowRoot.getElementById('footprint')
   }
 
   // Fires when an instance was inserted into the document.
@@ -35,23 +28,6 @@ class Pile extends window.HTMLElement {
   }
 
   init () {
-    // this.style.width = `${this.width}px`
-    // this.style.height = `${this.height}px`
-    // this.containerId = this.getAttribute('id')
-
-    // TODO: cardList is on table. store a internal _cardList to check for
-    // changes on cardlest when needing to render()? no
-    // OR use a Proxy when creating the cards? yes
-
-    this.addEventListener('crdtrx-card-pileset', function (ev) {
-      console.log('table pileset', ev.type, ev.composedPath())
-      if (ev.detail.pileId !== this.id) {
-        // card no longer in this pile
-        this.removeCard(ev.detail.cardId)
-      }
-      // TODO: determine what pile that the card is going to?
-      // If it is within a pile bounds then update card pile to that one and set the maxZIndex.
-    }, false)
   }
 
   // Fires when an attribute was added, removed, or updated.
@@ -70,17 +46,23 @@ class Pile extends window.HTMLElement {
   }
 
   render () {
+    /* TODO: should there be any styles for the pile set here?
     this.footprint.style.width = `${this.width}px`
     this.footprint.style.height = `${this.height}px`
     this.footprint.style.transform = `translate3d(${this.x}px, ${this.y}px, 0)`
+    */
   }
 
   static get name () {
     return 'crdtrx-pile'
   }
 
+  static randomListOfNumbers (count) {
+    return fisherYates(count)
+  }
+
   shuffle (numberOfCards) {
-    const list = fisherYates(numberOfCards)
+    const list = Pile.randomListOfNumbers(numberOfCards)
     const pileShuffleEvent = new window.CustomEvent('crdtrx-pile-shuffle', {
       bubbles: true,
       composed: true,
