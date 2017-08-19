2f6e25837da1958dcf416543eb3a8506a50b091a
Jake Hickenlooper <jake@weboftomorrow.com>
2017-08-19 07:34:03 -0600

    Add initial example of all components
    Just copied over the card example to start with.

---
+++ example/example.js
@@ -0,0 +1,81 @@
+const crdtrxCard = 'crdtrx-card'
+const tableElement = document.getElementById('example-card-tricks-table')
+const spotElement = document.getElementById('spot')
+
+window.customElements.whenDefined(crdtrxCard)
+  .then(() => {
+    return window.customElements.get(crdtrxCard)
+  })
+  .then((Card) => {
+    // Card is now defined and could be used to create card elements.
+
+    // Delay setting impetus friction until after the initial transition. This way it can slide in to begin with.
+    const cardList = document.querySelectorAll(crdtrxCard)
+    cardList.forEach((card) => {
+      // Changing the friction will reinit the impetus for the card. Any
+      // non-number value, like false, will just use the default friction for
+      // impetus.
+      card.addEventListener('transitionend', finished, true)
+      function finished (ev) {
+        card.setAttribute('friction', true)
+        card.removeEventListener('transitionend', finished)
+      }
+
+      // Set flip button action
+      const flipButton = card.querySelector('[data-action-flip]')
+      if (flipButton) {
+        flipButton.addEventListener('click', function handleClickFlipButton (ev) {
+          flipCard(card)
+        }, true)
+      }
+
+      // Set rotate button action
+      const rotateButton = card.querySelector('[data-action-rotate]')
+      if (rotateButton) {
+        rotateButton.addEventListener('click', function handleClickRotateButton (ev) {
+          rotateCard(card)
+        }, true)
+      }
+    })
+
+    tableElement.addEventListener('crdtrx-card-xy', function (ev) {
+      // Update the spot element position everytime the card is at a final position.
+      updateSpotPosition(ev.detail.x, ev.detail.y)
+    }, false)
+  })
+
+function flipCard (card) {
+  card.setAttribute('side', card.getAttribute('side') === 'front' ? 'back' : 'front')
+}
+function rotateCard (card) {
+  card.setAttribute('r', Number(card.getAttribute('r')) + 20)
+}
+
+function updateSpotPosition (x, y) {
+  spotElement.style.transform = `translate3d(${x}px, ${y}px, 0)`
+}
+
+// Stack all button example
+document.getElementById('stack-all').addEventListener('click', function (ev) {
+  const cardList = document.querySelectorAll(crdtrxCard)
+  cardList.forEach((card, index) => {
+    card.addEventListener('transitionend', finished, true)
+    // transitionend event can be cancelled so use a fallback timeout that is
+    // +100ms for the transition duration
+    const timeoutId = window.setTimeout(finished, 400)
+    // disable impetus
+    card.removeAttribute('friction')
+
+    // Set the new position which will transition
+    card.x = 100
+    card.y = 200
+    card.z = index
+    function finished (ev) {
+      window.clearTimeout(timeoutId)
+      // Add impetus back in
+      card.setAttribute('friction', true)
+      card.removeEventListener('transitionend', finished)
+      updateSpotPosition(card.x, card.y)
+    }
+  })
+})
